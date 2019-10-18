1f82c5c41fa8b411b3bd6c12ba59f7668bdc0ad7
ngarcia11 <norlangarcia402@gmail.com>
2019-10-18 15:10:24 -0600

    2 files modified

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import { Usuarios } from './usuarios';
 import { UsersService } from './users.service';
 
