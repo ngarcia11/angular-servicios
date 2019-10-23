72aff851c04bfec8c478baadc8a76a6e874a2d15
ngarcia11 <norlangarcia402@gmail.com>
2019-10-23 08:41:20 -0600

    2

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuarios } from './usuarios';
-import { UsersService } from './users.service';
+
 
 @Component({
   selector: "app-user",
   templateUrl: "./user.component.html",
   styleUrls: ["./user.component.css"]
 })
-export class UserComponent implements OnInit {
+export class UserComponent {
   u: any[] = [];
-
-  constructor(public uS : UsersService) {}
 
   users: Usuarios[] = [
     {
@@ -29,15 +27,4 @@ export class UserComponent implements OnInit {
       address: "Tegucigalpa"
     }
   ];
-  ngOnInit() {
-     this.uS.getUser.subscribe(
-      data => {
-        // Success
-        this.u = data["results"];
-      },
-      error => {
-        console.error(error);
-      }
-    );
-  }
 }
